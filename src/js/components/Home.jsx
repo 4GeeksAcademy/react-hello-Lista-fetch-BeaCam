4c9a1ed00c8b7dab2eec4506e7d9f1810fa9c586
beatriz24BCN <beatriz24bcn@hotmail.com>
2026-03-24 10:53:36 +0000

    projecto terminado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,117 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const API_TODO = "https://playground.4geeks.com/todo";
+const USER = "beatriz24BCN";
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+  const [tasks, setTasks] = useState([]);
+  const [form, setForm] = useState({
+    label: ""
+  });
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  useEffect(() => {
+    obtenerTareas();
+  }, []);
+
+  const obtenerTareas = () => {
+    fetch(`${API_TODO}/users/${USER}`)
+      .then((response) => {
+        if (!response.ok) {
+          return crearUsuario().then(() => obtenerTareas());
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data) setTasks(data.todos || []);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const crearUsuario = () => {
+    return fetch(`${API_TODO}/users/${USER}`, {
+      method: "POST"
+    });
+  };
+
+  const crearTarea = (tarea) => {
+    fetch(`${API_TODO}/todos/${USER}`, {
+      method: "POST",
+      body: JSON.stringify({
+        label: tarea.label,
+        is_done: false
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(() => {
+        obtenerTareas();
+        setForm({ label: "" });
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const eliminarTarea = (id) => {
+    fetch(`${API_TODO}/todos/${id}`, {
+      method: "DELETE"
+    })
+      .then(() => {
+        setTasks((tareas) =>
+          tareas.filter((tarea) => tarea.id !== id)
+        );
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const limpiarTareas = () => {
+    fetch(`${API_TODO}/users/${USER}`, {
+      method: "DELETE"
+    })
+      .then(() => {
+        setTasks([]);
+        crearUsuario();
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    crearTarea(form);
+  };
+
+  const handleChange = (e) => {
+    setForm({ label: e.target.value });
+  };
+
+  return (
+    <div className="container">
+      <h1>📝 Todo List</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Nueva tarea"
+          value={form.label}
+          onChange={handleChange}
+          required
+        />
+        <button>Agregar</button>
+      </form>
+
+      <ul>
+        {tasks.map((tarea) => (
+          <li key={tarea.id}>
+            {tarea.label}
+            <button onClick={() => eliminarTarea(tarea.id)}>
+              ❌
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <button onClick={limpiarTareas}>🧹 Limpiar todo</button>
+    </div>
+  );
 };
 
 export default Home;
